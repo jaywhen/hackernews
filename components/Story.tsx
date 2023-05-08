41fbec45dbac8b0c8b70f0321fa7da3eae32b6b7
jaywhen <jaywhenx@gmail.com>
2023-05-09 01:06:17 +0800

    fix: when click outside of the modal not close

--- components/Story.tsx
+++ components/Story.tsx
@@ -40,35 +40,37 @@ const Story = (story: StoryData) => {
   };
 
   return (
-    <div
-      onClick={onClickStory}
-      className="p-2 my-4 flex justify-between lg:hover:shadow-md lg:hover:shadow-[#ff66004c] lg:hover:cursor-pointer lg:hover:-translate-y-1 lg:hover:scale-110 hover:rounded-[15px] transition ease-in-out delay-50 duration-300"
-    >
-      <div className="w-4/5">
-        <div className="w-11/12 font-bold text-lg break-words">
-          <a href={story.url} target="_blank" rel="noreferrer">
-            {story.title}
-          </a>
+    <>
+      <div
+        onClick={onClickStory}
+        className="p-2 my-4 flex justify-between lg:hover:shadow-md lg:hover:shadow-[#ff66004c] lg:hover:cursor-pointer lg:hover:-translate-y-1 lg:hover:scale-110 hover:rounded-[15px] transition ease-in-out delay-50 duration-300"
+      >
+        <div className="w-4/5">
+          <div className="w-11/12 font-bold text-lg break-words">
+            <a href={story.url} target="_blank" rel="noreferrer">
+              {story.title}
+            </a>
+          </div>
+          <div className="text-sm">
+            <span className="text-[#FF6600] font-bold">{story.score}</span>
+            &nbsp;points by&nbsp;
+            <span className="hover:underline">
+              <Link href={`/user/${story.by}`}>{story.by}</Link>
+            </span>
+            {` • (`}
+            <a
+              className="hover:underline"
+              href={`https://${story.hostname}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {story.hostname}
+            </a>
+            {`)`}
+          </div>
         </div>
-        <div className="text-sm">
-          <span className="text-[#FF6600] font-bold">{story.score}</span>
-          &nbsp;points by&nbsp;
-          <span className="hover:underline">
-            <Link href={`/user/${story.by}`}>{story.by}</Link>
-          </span>
-          {` • (`}
-          <a
-            className="hover:underline"
-            href={`https://${story.hostname}`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            {story.hostname}
-          </a>
-          {`)`}
-        </div>
+        <div className="text-gray-600 font-bold">{story.time}</div>
       </div>
-      <div className="text-gray-600 font-bold">{story.time}</div>
       <Transition appear show={isOpen} as={Fragment}>
         <Dialog
           as="div"
@@ -148,7 +150,7 @@ const Story = (story: StoryData) => {
           </div>
         </Dialog>
       </Transition>
-    </div>
+    </>
   );
 };
 
